dd217ed302da4436ac4bb4789cce116ac4558b8e
Chen Dai <chendai@udel.edu>
2019-09-11 14:57:00 -0400

    add chen_hello.c

---
+++ hw1/chen_hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  // print string 10 times
+  for(int i = 0; i < 10; i++) {
+    printf("%d Hello from Chen Dai!\n", i+1);
+  }
+  //  int a[1000000];
+  // printf("%ld", sizeof(a));
+  return 0;
+}
